571e71d5dcf83e2cd447eda0b879c6add4484480
Frank151001 <121843098+Frank151001@users.noreply.github.com>
2023-02-27 21:29:45 -0800

    Assignment#3

--- scripts/app.js
+++ scripts/app.js
@@ -19,8 +19,45 @@ function toggleImportant(){
     }
 }
 
+function saveTask(){
+    let title=$("#txtTittle").val();
+    let desc=$("#txtDesc").val();
+    let category=$("#selCategory").val();
+    let dueDate=$("#selDueDate").val();
+    let priority=$("#selPriority").val();
+    let color=$("#selColor").val();
+    console.log(title,desc,category,dueDate,priority,color);
+
+    let task=new Task(isImportant,title,desc,category,dueDate,priority,color);
+    console.log(task);
+    displayTask(task);
+}
+
 function toggleView(){
     
+}
+
+function displayTask(newTask){
+    let icon="";
+    if(newTask.isImportant){
+        icon=  `<i class='${ImportantIcon}'></i>`;
+    } else{
+        icon=`<i class='${notImportantIcon}'></i>`;
+    }
+    
+
+    let syntax= `<div class="task" style=""border:2px solid ${newTask.color}>
+        <div class="info">
+            <h1>${newTask.title}</h1>
+            <p>${newTask.description}</p>
+        </div>
+        <label>${newTask.category}</label>
+        <div class="details">
+            <label>${newTask.priority}</label>
+            <label>${newTask.dueDate}</label>
+        </div>
+    </div>`;
+    $("#pending-tasks").append(syntax);
 }
 
  
@@ -29,6 +66,8 @@ function toggleView(){
 function init(){
     console.log("Task Manager");
     $("#formIcon").click(toggleImportant);
+    $("#toggleView").click(toggleView);
+    $("#btnSave").click(saveTask);
     
 }
 window.onload=init;
